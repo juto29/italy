e8be7613be78e54b131a1d1c466d96b77aebf750
juto29 <75456416+juto29@users.noreply.github.com>
2025-08-28 10:21:58 +0900

    Configure build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // Disable automatic browser opening for compatibility in headless environments
 export default defineConfig({
+  base: '/italy/',
   plugins: [react()],
   server: {
     port: 5173,
+  },
+  build: {
+    outDir: 'docs',
   },
 })

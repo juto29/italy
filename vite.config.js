e7c119db0a079fdc1725fcdc0698b6fd6a6a2a83
juto29 <75456416+juto29@users.noreply.github.com>
2025-08-28 10:58:55 +0900

    feat: configure base path via env

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()], server: { port: 5173, open: true } })
+// Load environment-specific variables to handle differing base paths
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [react()],
+    server: {
+      port: 5173,
+    },
+    build: {
+      outDir: 'docs',
+    },
+  }
+})
